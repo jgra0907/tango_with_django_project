7265e07ae20d866d1cc4c5c0a69e0d78dbbea857
Jakub <2400144g@student.gla.ac.uk>
2020-01-26 00:41:07 +0000

    Completed 3.5

--- rango/views.py
+++ rango/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Rango says hey there partner!")
+
+def a(request):
+    return HttpResponse("<h1>This is a test!</h1>")
