2d637cbda268e033fbf580a3832582ac931b1a43
Jakub <2400144g@student.gla.ac.uk>
2020-02-07 20:10:22 +0000

    Completed Chapter 7, sans Exercises

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 
 def index(request):
@@ -34,3 +36,20 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    # A HTTP POST?
+    if request.method=='POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            #The supplied form contained errors. Print them
+            print(form.errors)
+    #Handle bad form, new form, or no form supplied cases.
+    return render(request, 'rango/add_category.html', {'form': form})
